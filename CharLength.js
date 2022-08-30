5d521b494c0d6870bdf62dff7df0593af58f4fae
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-08-29 21:40:50 -0400

    Update CharLength.js

--- CharLength.js
+++ CharLength.js
@@ -4,4 +4,5 @@ var first = prompt('What is your first name?');
 var last = prompt('What is your last name?');
 var totalLength = first.length + last.length;
 
-
+alert('The number of characters in your name is ' + totalLength);
+alert('Your initials are ' + first.charAt(0) + last.charAt(0));
