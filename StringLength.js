a88d930835870026d4acb4a07e34e2321a726fdb
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-08-29 22:23:32 -0400

    Update StringLength.js

--- StringLength.js
+++ StringLength.js
@@ -7,3 +7,15 @@ prompt(country = 'Which country do you live in?');
 var country = prompt('What country do you live in?');
 if (country.length >=2 && country.length <=62) {
   alert('Thank you!');
+ 
+ //Using length & branches to check for input
+ //codeName must be 3 letters
+ var codeName = prompt('Choose your codename');
+
+if (codeName.length < 3) {
+	alert('Your codename is too short');
+} else if (codeName.length > 3) {
+	alert('Your codename is too long');
+} else if (codeName.length = 3) {
+ alert('Your codename is perfect length!)
+}
