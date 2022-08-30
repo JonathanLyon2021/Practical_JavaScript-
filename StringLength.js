ab44fa8bcbe6214e3e3ad876d2be7ae726861cfc
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-08-29 22:11:58 -0400

    Update StringLength.js

--- StringLength.js
+++ StringLength.js
@@ -1,3 +1,9 @@
  //String Length
 prompt(country = 'Which country do you live in?');
  alert(country.length);
+
+////Checking valid input with length method
+
+var country = prompt('What country do you live in?');
+if (country.length >=2 && country.length <=62) {
+  alert('Thank you!');
