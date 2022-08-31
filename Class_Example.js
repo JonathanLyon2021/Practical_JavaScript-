55e1badbbbdb29c6da0b9970e2ce18ce61012a84
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-08-31 15:01:30 -0400

    Update Class_Example.js

--- Class_Example.js
+++ Class_Example.js
@@ -1,10 +1,35 @@
+//Pen constructor function   
+///with name, color, price properties
 
-
-function Pen(name, color, price) {
+function Pen(name, color, price, length) {
     this.name = name;
     this.color = color;
     this.price = price;
+    this.length = length;
 }
 
-const pen1 = new Pen("Marker", "Blue", "$3");
-console.log(pen1);
+//"new" keyword creats a new object - pen1
+const pen2 = new Pen("BIC", "red", "$2", "5'");
+const pen1 = new Pen("Marker", "Blue", "$3", "5.5'");
+
+Pen.prototype.showPrice = function(){
+    console.log(`Price of ${this.name} is ${this.price}. Length is ${this.length}`);
+}
+
+console.log(pen2.color);
+pen1.showPrice();
+pen2.showPrice();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
