135b2c935d058a5c2f84805585f0e4999307a4c3
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-08-31 22:13:28 -0400

    Update Class_Example2.js
    Added two new variables for Classmate example classes.

--- Class_Example2.js
+++ Class_Example2.js
@@ -5,3 +5,7 @@ function Classmate(name, age, height, weight, sex) {
     this.weight = weight;
     this.sex = sex;
 }
+
+const person2 = new Classmate("Jonathan", 36, 5.11, 200, "Male");
+const person1 = new Classmate("Buggie", 19, 5.4, 115, "Female");
+
