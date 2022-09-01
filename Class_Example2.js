da489e29833862dedfed7aa1a3e76f5ead0b3f37
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-08-31 21:58:02 -0400

    Create Class_Example2.js

---
+++ Class_Example2.js
@@ -0,0 +1,10 @@
+
+class ClassMates{
+	constructor(name,age){
+    	this.name = name;
+      	this.age = age;
+    }
+}
+
+let classmate = new ClassMates("Mike Will",15);
+//classmate.displayInfo();  // result: Mike Will is 15 years old!
