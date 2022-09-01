b91f2e316b9eb3dafc68b400150a34e21310ce54
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-08-31 22:15:24 -0400

    Update Class_Example2.js

--- Class_Example2.js
+++ Class_Example2.js
@@ -12,3 +12,9 @@ const person1 = new Classmate("Buggie", 19, 5.4, 115, "Female");
 Classmate.prototype.showVitals = function(){
     console.log(`Student ${this.name} is ${this.height} tall, ${this.weight} lbs, ${this.age} old, and is a ${this.sex}.`);
 }
+
+console.log(person1.age);
+console.log(person2.sex);
+person1.showVitals();
+person2.showVitals();
+
