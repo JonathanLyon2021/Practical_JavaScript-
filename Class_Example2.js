cb38081855aa0c1f7cac6e6de5b07df9d2e6ff23
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-08-31 22:19:07 -0400

    Update Class_Example2.js

--- Class_Example2.js
+++ Class_Example2.js
@@ -6,8 +6,8 @@ function Classmate(name, age, height, weight, sex) {
     this.sex = sex;
 }
 
-const person2 = new Classmate("Jonathan", 36, 5.11, 200, "Male");
-const person1 = new Classmate("Buggie", 19, 5.4, 115, "Female");
+const person1 = new Classmate("Jonathan", 36, 5.11, 200, "Male");
+const person2 = new Classmate("Buggie", 19, 5.4, 115, "Female");
 
 Classmate.prototype.showVitals = function(){
     console.log(`Student ${this.name} is ${this.height} tall, ${this.weight} lbs, ${this.age} years old, and is a ${this.sex}.`);
@@ -15,6 +15,7 @@ Classmate.prototype.showVitals = function(){
 
 console.log(person1.age);
 console.log(person2.sex);
+console.log(person2.name);
 person1.showVitals();
 person2.showVitals();
 
@@ -22,6 +23,8 @@ person2.showVitals();
 /*Classes.js:16*/
 //Female
 /*Classes.js:17*/
+//Buggie
+/*Classes.js:18*/
 //Student Jonathan is 5.11 tall, 200 lbs, 36 years old, and is a Male.
 /*Classes.js:13*/
 //Student Buggie is 5.4 tall, 115 lbs, 19 years old, and is a Female.
