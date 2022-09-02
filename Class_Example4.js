307b02816d08a466e0061cd026af76488ed14dab
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-09-01 21:20:56 -0400

    Update Class_Example4.js

--- Class_Example4.js
+++ Class_Example4.js
@@ -7,3 +7,12 @@ let Rectangle = class {
   console.log(Rectangle.name, );
   // output: "Rectangle"
   
+  // named
+  Rectangle = class Rectangle2 {
+    constructor(height, width) {
+      this.height = height;
+      this.width = width;
+    }
+  };
+  console.log(Rectangle.name);
+  // output: "Rectangle2"
