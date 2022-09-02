cc595bb20ce8989df05754cfb349ee4970115acc
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-09-01 21:52:07 -0400

    Update Class_Example5.js

--- Class_Example5.js
+++ Class_Example5.js
@@ -2,3 +2,9 @@ class Polygon {
     constructor(...sides) {
       this.sides = sides;
     }
+ // Method
+    *getSides() {
+      for (const side of this.sides) {
+        yield side;
+      }
+    }
