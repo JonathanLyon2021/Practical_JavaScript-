674a5ae19f554cda384fb11c33790a01c2db5d2d
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-09-01 21:54:33 -0400

    Added class Point

--- Class_Example5.js
+++ Class_Example5.js
@@ -13,3 +13,14 @@ class Polygon {
   const pentagon = new Polygon(1,2,3,4,5);
   
   console.log([...pentagon.getSides()]); // [1,2,3,4,5]
+/////////////////////////////////////////////////////////
+/*Static members (properties and methods) are called without instantiating their class and cannot be called 
+through a class instance. Static methods are often used to create utility functions for an application.*/
+
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+ 
