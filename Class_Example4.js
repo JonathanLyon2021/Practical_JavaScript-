6c8a309e86c7f88d3f22ac99f8d705135786dcfb
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-09-01 21:20:35 -0400

    Create Class_Example4.js

---
+++ Class_Example4.js
@@ -0,0 +1,9 @@
+let Rectangle = class {
+    constructor(height, width) {
+      this.height = height;
+      this.width = width;
+    }
+  };
+  console.log(Rectangle.name, );
+  // output: "Rectangle"
+  
